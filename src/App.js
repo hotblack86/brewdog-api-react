c3cb89dae4a649da8312789cda771bf5663ca438
James Hotblack <james.c.hotblack@gmail.com>
2019-09-18 16:11:51 +0100

    Added an async function: getBeers that fetches data from API. Using reacts useEffect to run function.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
   const allBeersURL = "https://api.punkapi.com/v2/beers?per_page=80"
 
+  useEffect( () => {
+    getBeers();
+  }, []);
+
+
+  const getBeers = async () => {
+    const response = await fetch(allBeersURL);
+    const data = await response.json();
+    console.log(data);
+  }
 
   return (
     <div className="App">
