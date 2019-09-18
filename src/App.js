eacf0ac314194586300a7bff008b581f79141f96
James Hotblack <james.c.hotblack@gmail.com>
2019-09-18 16:02:44 +0100

    Stored API URL and set up html search functionality.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+
+  const allBeersURL = "https://api.punkapi.com/v2/beers?per_page=80"
+
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+       <input className="search-bar" type="text" />
+       <button className="search-button" type="submit">
+         Search
+       </button>
+     </form>
     </div>
   );
 }
