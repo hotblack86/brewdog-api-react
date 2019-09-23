d1df345b5662e4de50a23bedf2d02af8d8514a0f
James Hotblack <james.c.hotblack@gmail.com>
2019-09-23 16:16:53 +0100

    Added Beer component CSS module

--- src/Beer.js
+++ src/Beer.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import style from './beer.module.css';
 
 
 const Beer = ({title, abv, description, image}) => {
   return(
-    <div >
+    <div className={style.beer}>
       <h1>{title}</h1>
       <h2>{abv}</h2>
       <p>{description}</p>
-      <img src={image} alt="" />
+      <img src={image} alt=""  />
     </div>
   )
 }
