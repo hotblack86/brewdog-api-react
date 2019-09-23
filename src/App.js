f66a46902902e7615d16e0b3576ada481be00066
James Hotblack <james.c.hotblack@gmail.com>
2019-09-23 16:00:16 +0100

    Attempting to implement onClick thatchanges API url.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import './App.css';
 
 const App = () => {
 
-  const allBeersURL = `https://api.punkapi.com/v2/beers?per_page=80`
-
+  
   const [beers, setBeers] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('Punk');
+  const [api, setAPI] = useState(`https://api.punkapi.com/v2/beers?beer_name=${query}`)
 
-  // ?beer_name=${search}
+
+
 
   useEffect( () => {
     getBeers();
-  }, []);
+  }, [query]);
 
+
+// ?beer_name=${query}
 
   const getBeers = async () => {
-    const response = await fetch(allBeersURL);
+    const response = await fetch(api);
     const data = await response.json();
-    
+    console.log(data)
     setBeers(data);
   }
 
@@ -27,11 +31,27 @@ const App = () => {
     setSearch(e.target.value);
   }
 
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  const showAllBeers = () => {
+    setAPI(`https://api.punkapi.com/v2/beers?per_page=80`)
+  }
+
+
   console.log(beers)
 
   return (
     <div className="App">
-      <form className="search-form">
+      <h1 className="title">Brewdog Beer Search App</h1>
+      <h3 className="title">Enter beer name below to see matches</h3>
+      
+      <button type="button" onClick={showAllBeers}>Show All Beers</button>
+      
+      <form onSubmit={getSearch} className="search-form">
        <input 
           className="search-bar" 
           type="text" 
